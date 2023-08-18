e8d02e0ad9bb7e066ea94abbf38eac87d722e83a
mik <ifinal.io@qq.com>
2023-08-18 14:01:31 +0800

    init

---
+++ final-nezha/nezha-web/src/main/java/org/ifinalframework/apps/nezha/web/controller/HelloController.java
@@ -0,0 +1,20 @@
+package org.ifinalframework.apps.nezha.web.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * HelloController
+ *
+ * @author mik
+ * @since 1.5.2
+ **/
+@RestController
+@RequestMapping("/api/hello")
+public class HelloController {
+    @GetMapping
+    public String hello(){
+        return "Hello!, I'm Nezha!";
+    }
+}
