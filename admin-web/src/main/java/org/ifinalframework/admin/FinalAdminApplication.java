730be3af25b852cd18828f479e943d74069e8c33
mik <ifinal.io@qq.com>
2023-10-01 20:48:13 +0800

    init

--- admin-web/src/main/java/org/ifinalframework/admin/FinalAdminApplication.java
+++ admin-web/src/main/java/org/ifinalframework/admin/FinalAdminApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinalNezhaApplication {
+public class FinalAdminApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FinalNezhaApplication.class, args);
+        SpringApplication.run(FinalAdminApplication.class, args);
     }
 
 }
