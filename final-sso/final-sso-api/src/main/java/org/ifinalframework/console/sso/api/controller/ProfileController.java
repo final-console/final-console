697ed304a043cff309fa9bd65ed7c4aefe77a9c2
mik <ifinal.io@qq.com>
2024-01-17 13:00:53 +0800

    add ProfileController

---
+++ final-sso/final-sso-api/src/main/java/org/ifinalframework/console/sso/api/controller/ProfileController.java
@@ -0,0 +1,24 @@
+package org.ifinalframework.console.sso.api.controller;
+
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ProfileController
+ *
+ * @author mik
+ * @since 1.5.6
+ **/
+@RestController
+@RequestMapping("/profile")
+public class ProfileController {
+    @GetMapping
+    @PreAuthorize("isAuthenticated()")
+    public Object profile() {
+        return SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+}
