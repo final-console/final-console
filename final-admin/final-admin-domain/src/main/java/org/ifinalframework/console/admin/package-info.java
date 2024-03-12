f6a777b87daca41dd1fd100ab5bfad2c62d287d9
iimik <iimik@qq.com>
2024-03-12 09:12:09 +0800

    update

---
+++ final-admin/final-admin-domain/src/main/java/org/ifinalframework/console/admin/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * package-info
+ *
+ * @author iimik
+ * @since 1.6.0
+ **/
+@AutoService
+@AutoQuery
+package org.ifinalframework.console.admin;
+
+import org.ifinalframework.data.auto.annotation.AutoQuery;
+import org.ifinalframework.data.auto.annotation.AutoService;
---
+++ final-admin/final-admin-repository/src/main/java/org/ifinalframework/console/admin/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * package-info
+ *
+ * @author iimik
+ * @since 1.6.0
+ **/
+@AutoMapper
+package org.ifinalframework.console.admin;
+
+import org.ifinalframework.data.auto.annotation.AutoMapper;
